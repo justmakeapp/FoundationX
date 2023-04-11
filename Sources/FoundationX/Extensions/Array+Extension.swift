aa971d12a056a6a2624741b5b213d0487bf9fdc3
Long Vu <long.vudai01@gmail.com>
2023-04-11 17:06:13 +0700

    add array extension

---
+++ Sources/FoundationX/Extensions/Array+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Array+Extension.swift
+//  
+//
+//  Created by Long Vu on 11/04/2023.
+//
+
+import Foundation
+
+public extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
