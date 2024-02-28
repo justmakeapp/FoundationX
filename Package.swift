ef59710d71863766124c33c124c4a0524780306a
Long Vu <long.vudai01@gmail.com>
2024-02-28 22:56:58 +0700

    support visionOS

--- Package.swift
+++ Package.swift
@@ -1,11 +1,15 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "FoundationX",
-    platforms: [.iOS(.v13), .macOS(.v11), .watchOS(.v7)],
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11),
+        .watchOS(.v7),
+        .visionOS(.v1)
+    ],
     products: [
         .library(
             name: "FoundationX",
