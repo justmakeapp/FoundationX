eb0d73d36a478461368aca2c0504be18436c3b56
Long Vu <long.vudai01@gmail.com>
2024-04-27 21:55:24 +0700

    add isValidURL

--- Sources/FoundationX/Extensions/String+Extension.swift
+++ Sources/FoundationX/Extensions/String+Extension.swift
@@ -19,3 +19,19 @@ public extension String {
         return regex.firstMatch(in: lhs, options: [], range: range) != nil
     }
 }
+
+public extension String {
+    var isValidURL: Bool {
+        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+        if let match = detector.firstMatch(
+            in: self,
+            options: [],
+            range: NSRange(location: 0, length: self.utf16.count)
+        ) {
+            // it is a link, if the match covers the whole string
+            return match.range.length == self.utf16.count
+        } else {
+            return false
+        }
+    }
+}
