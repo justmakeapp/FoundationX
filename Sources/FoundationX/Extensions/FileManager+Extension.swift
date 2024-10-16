e2d76fb0c6511e98bd782fed00170eb7c01bb1cd
Long Vu <long.vudai01@gmail.com>
2024-10-16 18:33:28 +0700

    add directoryExistsAtPath

--- Sources/FoundationX/Extensions/FileManager+Extension.swift
+++ Sources/FoundationX/Extensions/FileManager+Extension.swift
@@ -9,6 +9,16 @@ import Foundation
 
 public extension FileManager {
     var documentURL: URL {
-        return urls(for: .documentDirectory, in: .userDomainMask)[0]
+        if #available(iOS 16.0, macOS 13.0, *) {
+            return URL.documentsDirectory
+        } else {
+            return urls(for: .documentDirectory, in: .userDomainMask)[0]
+        }
+    }
+
+    func directoryExistsAtPath(_ path: String) -> Bool {
+        var isDirectory: ObjCBool = true
+        let exists = FileManager.default.fileExists(atPath: path, isDirectory: &isDirectory)
+        return exists && isDirectory.boolValue
     }
 }
