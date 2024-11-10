c51fd1aadbbf87583d45c946898fc3754e38b5b4
Long Vu <long.vudai01@gmail.com>
2024-11-10 17:27:29 +0700

    Update swift-tools-version to 6.0 in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 
 import PackageDescription
 
@@ -17,6 +17,9 @@ let package = Package(
         ),
     ],
     targets: [
-        .target(name: "FoundationX"),
-    ]
+        .target(
+            name: "FoundationX"
+        ),
+    ],
+    swiftLanguageModes: [.v6]
 )
